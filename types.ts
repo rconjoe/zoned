8c7be1ccdb022caaf0549ead743da2869435f4c2
rconjoe <joe@rconjoe.com>
2021-12-14 19:25:54 -0800

    init types file ft. ZonerOptions and Format

---
+++ types.ts
@@ -0,0 +1,69 @@
+export interface ZonerOptions {
+
+  /**
+   * Allows you to specify a timezone for this instance, e.g. if you already store
+   * a timezone for each user in a database. Must be in Area/Location form (e.g. America/New_York).
+   * If not set, will be guessed by dayjs.
+   */
+  timezone?: string;
+
+  /**
+   * Date format which all non-specified conversion outputs will default to.
+   */
+  format?: Format;
+
+}
+
+export enum Format {
+
+  /**
+   * 8:02 PM
+   */
+  'h:mm A', 
+
+  /**
+   * 8:02:18 PM
+   */
+  'h:mm:ss A',
+
+  /**
+   * 08/16/2018
+   */
+  'MM/DD/YY',
+
+  /**
+   * August 16, 2018
+   */
+  'MMMM D, YYYY',
+
+  /**
+   * August 16, 2018, 8:02 PM
+   */
+  'MMMM D, YYYY h:mm A',
+
+  /**
+   * Thursdau, August 16, 2018, 8:02 PM
+   */
+  'dddd, MMMM D, YYYY h:mm A',
+
+  /**
+   * 8/16/2018
+   */
+  'M/D/YYYY',
+
+  /**
+   * Aug 16, 2018
+   */
+  'MMM D YYYY',
+
+  /**
+   * Aug 16, 2018 8:02 PM
+   */
+  'MMM D YYYY h:mm A',
+
+  /**
+   * Thu, Aug 16, 2018 8:02 PM
+   */
+  'ddd, MMM D, YYYY h:mm A'
+
+}
