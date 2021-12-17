d7f5112752b36937bf4fc6cf392bd27cb4ca4e61
rconjoe <joe@rconjoe.com>
2021-12-17 11:30:11 -0800

    main controller class

--- lib/Zoned.ts
+++ lib/Zoned.ts
@@ -1,29 +1,47 @@
-import dayjs, { Dayjs } from 'dayjs'
-import utc from 'dayjs/plugin/utc'
-import timezone from 'dayjs/plugin/timezone'
 import Unix from './Unix'
 import Format from './Format'
+import dayjs, { Dayjs } from 'dayjs'
 
 import {
   ZonedOptions,
 } from '../types'
 
-
-dayjs.extend(utc)
-dayjs.extend(timezone)
-
-
+/**
+ * @name Zoned
+ * @description Main controller class. This class is instantiated by the user in their app.
+ * @property options <ZonedOptions> App options. See ZonedOptions interface.
+ * @property unix <Unix> An instance of this app's Unix class, instantiated in the Zoned constructor.
+ * @property format <Format> An instance of this app's Format class, instantiated in the Zoned constructor.
+ *
+ * @export
+ * @class
+ */
 export default class {
 
-  public options?: ZonedOptions;
+  private options?: ZonedOptions;
   public unix: Unix;
   public format: Format;
 
   constructor(options?: ZonedOptions) {
     this.options = options
-    this.format = new Format(options?.format)
-    this.unix = new Unix()
+    this.format = new Format(options?.format, options?.timezone)
+    this.unix = new Unix(options?.javascriptSeconds)
   }
 
+
+  public engine(input: any): Dayjs {
+    return dayjs(input).tz(this.options?.timezone)
+  }
+
+
+  public formatDate(date?: Date): string {
+    if (!date) date = new Date()
+    return this.format.date(date)
+  }
+
+  public formatUnix(seconds?: number): string {
+    if (!seconds) seconds = this.unix.fromJsDate(new Date())
+    return this.format.date(this.unix.toJsDate(seconds))
+  }
 
 }
