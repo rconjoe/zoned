c55d1aa5d7ff26831e89f06ef7921ff74be784d9
rconjoe <joe@rconjoe.com>
2021-12-17 11:30:01 -0800

    unix class to convert to/from seconds values

--- lib/Unix.ts
+++ lib/Unix.ts
@@ -1,9 +1,23 @@
-import dayjs from 'dayjs'
-
 export default class {
 
+  javascriptSeconds: boolean
+  seconds?: number
+
+  constructor(javascriptSeconds?: boolean) { 
+    this.javascriptSeconds = javascriptSeconds ? javascriptSeconds : false
+  }
+
+  private set(seconds: number): number {
+    return this.seconds = this.javascriptSeconds ? seconds * 1000 : seconds
+  }
+
   public toJsDate(seconds: number): Date {
-    return new Date(seconds! * 1000)
+    this.seconds = this.set(seconds)
+    return new Date(this.seconds! * 1000)
+  }
+
+  public fromJsDate(date: Date): number {
+    return Math.round(date.getTime() / 1000)
   }
 
 }
